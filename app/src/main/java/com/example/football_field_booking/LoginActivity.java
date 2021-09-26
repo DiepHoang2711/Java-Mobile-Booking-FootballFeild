3a228a2798fd4996dda29160553b9c58d3c66d11
Tien Xuan <tienxuan0404@gmail.com.vn>
2021-09-26 22:30:34 +0700

    create function add new user's data to DB

--- app/src/main/java/com/example/football_field_booking/LoginActivity.java
+++ app/src/main/java/com/example/football_field_booking/LoginActivity.java
@@ -16,17 +16,27 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final String GOOGLE_LOG = "Google Log:";
     private static final int RC_SIGN_IN = 9001;
+    public static final String COLLECTION_USERS = "users";
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
 
@@ -73,11 +83,11 @@ public class LoginActivity extends AppCompatActivity {
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-                Log.d("Google", "firebaseAuthWithGoogle:" + account.getId());
+                Log.d(GOOGLE_LOG, "firebaseAuthWithGoogle:" + account.getId());
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
-                Log.w("Google", "Google sign in failed", e);
+                Log.w(GOOGLE_LOG, "Google sign in failed", e);
             }
         }
 
@@ -91,17 +101,38 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d("Google", "signInWithCredential:success");
+                            Log.d(GOOGLE_LOG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
 
-                            if(task.getResult().getAdditionalUserInfo().isNewUser()){
+                            if (task.getResult().getAdditionalUserInfo().isNewUser()) {
                                 // ADD to database
+                                DocumentReference doc = db.collection(COLLECTION_USERS).document();
 
+                                UserDTO userDTO = new UserDTO(doc.getId(), user.getEmail(),
+                                        user.getDisplayName(), user.getPhoneNumber(), "user",
+                                        "active",user.getPhotoUrl().toString());
+
+                                Map<String, Object> userDB = new HashMap<>();
+                                userDB.put("userInfo", userDTO);
+
+                                doc.set(userDB)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            Log.d(GOOGLE_LOG, "DocumentSnapshot added with ID: " + doc.getId());
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(GOOGLE_LOG, "Error adding document", e);
+                                        }
+                                    });
                             }
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w("Google", "signInWithCredential:failure", task.getException());
+                            Log.w(GOOGLE_LOG, "signInWithCredential:failure", task.getException());
                             updateUI(null);
                         }
                     }
@@ -115,7 +146,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void updateUI(FirebaseUser user) {
-        if(user!=null){
+        if (user != null) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
         }
