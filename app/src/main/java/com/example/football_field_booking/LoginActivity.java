1d396266431ab89ac8fdd8c0917e80b2c5a0d1c7
Tien Xuan <tienxuan0404@gmail.com.vn>
2021-09-28 13:55:30 +0700

    create class validate

--- app/src/main/java/com/example/football_field_booking/LoginActivity.java
+++ app/src/main/java/com/example/football_field_booking/LoginActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.football_field_booking.DTO.UserDTO;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
